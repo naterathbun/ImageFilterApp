4f49453eb02985b8e7b6a9728dcaef41ce813d02
Nathan Rathbun <naterathbun@gmail.com>
2017-02-03 13:53:17 -0500

    Updated layout and file loading

--- ImageFilterApp/MainWindow.xaml.cs
+++ ImageFilterApp/MainWindow.xaml.cs
@@ -12,17 +12,50 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace ImageFilterApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public string ImageFilePath { get; set; }
+        public string FilePathOfWorkingDirectory
+        {
+            get { return System.IO.Path.GetDirectoryName(ImageFilePath); }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+
+        private void OpenFileBrowserToSelectImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+            GetFilePathOfImage(openFileDialog);
+            SetImageFromFile();
+
+        }
+
+        private void GetFilePathOfImage(OpenFileDialog openFileDialog1)
+        {
+            ImageFilePath = openFileDialog1.FileName;
+        }
+
+        private void SetImageFromFile()
+        {
+            imagePreview.Source = new BitmapImage(new Uri(ImageFilePath));
+            newImagePreview.Source = new BitmapImage(new Uri(ImageFilePath));
+        }
+
+        private void UpdateImagePreview()
+        {
+
+        }
+
     }
 }
