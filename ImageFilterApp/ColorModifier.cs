a12a630d085049ea5bf96a2cde8c1d7db8315b3a
Nathan Rathbun <naterathbun@gmail.com>
2017-02-03 17:42:53 -0500

    Basic functionality complete, need to add save feature

---
+++ ImageFilterApp/ColorModifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageFilterApp
+{
+    public class ColorModifier
+    {
+        public int Red { get; private set; }
+        public int Green { get; private set; }
+        public int Blue { get; private set; }
+
+        public ColorModifier(int red, int green, int blue)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+        }
+    }
+}
--- ImageFilterApp/MainWindow.xaml.cs
+++ ImageFilterApp/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace ImageFilterApp
         }
 
 
-        private void Colorizer()
+        private void ChangeImageColors(ColorModifier newColorInfo)
         {
             byte[] imageInBytes = File.ReadAllBytes(ImageFilePath);
 
@@ -48,11 +48,14 @@ namespace ImageFilterApp
                         for (int yCoordinate = 0; yCoordinate < previewImageBitMap.Height; yCoordinate++)
                         {
                             System.Drawing.Color oldPixelColor = previewImageBitMap.GetPixel(xCoordinate, yCoordinate);
-                            previewImageBitMap.SetPixel(xCoordinate, yCoordinate, System.Drawing.Color.FromArgb(oldPixelColor.R, oldPixelColor.G, 0));
+                            System.Drawing.Color newColor = GetNewPixelColor(oldPixelColor, newColorInfo);
+
+                            previewImageBitMap.SetPixel(xCoordinate, yCoordinate, newColor);
                         }
                     }
 
                     UpdateImagePreview(previewImageBitMap);
+                    return previewImageBitMap;
                 }
             }
         }
@@ -69,6 +72,10 @@ namespace ImageFilterApp
             return image;
         }
 
+        private void UpdateImagePreview(Bitmap previewImageBitMap)
+        {
+            newImagePreview.Source = ConvertBitMapToImageSource(previewImageBitMap);
+        }
 
 
         private void OpenFileBrowserToSelectImage(object sender, RoutedEventArgs e)
@@ -91,14 +98,56 @@ namespace ImageFilterApp
             newImagePreview.Source = new BitmapImage(new Uri(ImageFilePath));
         }
 
-        private void UpdateImagePreview(Bitmap previewImageBitMap)
+
+        public System.Drawing.Color GetNewPixelColor(System.Drawing.Color oldColor, ColorModifier colorModifier)
         {
-            newImagePreview.Source = ConvertBitMapToImageSource(previewImageBitMap);
+            int newRed = Convert.ToInt32(oldColor.R + (colorModifier.Red * 2.5));
+            int newGreen = Convert.ToInt32(oldColor.G + (colorModifier.Green * 2.5)); ;
+            int newBlue = Convert.ToInt32(oldColor.B + (colorModifier.Blue * 2.5)); ; ;
+
+            if (newRed > 255)
+                newRed = 255;
+            if (newRed < 0)
+                newRed = 0;
+            if (newGreen > 255)
+                newGreen = 255;
+            if (newGreen < 0)
+                newGreen = 0;
+            if (newBlue > 255)
+                newBlue = 255;
+            if (newBlue < 0)
+                newBlue = 0;
+
+            System.Drawing.Color newColor = System.Drawing.Color.FromArgb(newRed, newGreen, newBlue);
+
+            return newColor;
+
         }
 
         private void SaveImageCopy(object sender, RoutedEventArgs e)
         {
+            string newImageFilePath = ImageFilePath;
 
+            while (File.Exists(newImageFilePath))
+            {
+                newImageFilePath = FilePathOfWorkingDirectory + @"\" + System.IO.Path.GetFileNameWithoutExtension(newImageFilePath) + "-COPY" + System.IO.Path.GetExtension(newImageFilePath);
+            }
+
+            // create a new file based on the bitmap in the right box
+            // this is the last step to be "working"
         }
+
+        public void applyToImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            int red = Convert.ToInt32(sliderRed.Value);
+            int green = Convert.ToInt32(sliderGreen.Value);
+            int blue = Convert.ToInt32(sliderBlue.Value);
+
+            ColorModifier newColorModifier = new ColorModifier(red, green, blue);
+            ChangeImageColors(newColorModifier);
+        }
+
+
+
     }
 }
