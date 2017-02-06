b60a76acd60068f8ac8c90c3e06ae1ac5c7642c9
Nathan Rathbun <naterathbun@gmail.com>
2017-02-06 09:18:09 -0500

    Added default file ext and fixed mouseover on buttons

--- ImageFilterApp/MainWindow.xaml.cs
+++ ImageFilterApp/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
 using System.Drawing;
@@ -138,6 +127,9 @@ namespace ImageFilterApp
         public void SaveImageCopy(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "jpg";
+            saveFileDialog.AddExtension = true;
+
             if (saveFileDialog.ShowDialog() == true)
             {
                 ModifiedBitmap.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
